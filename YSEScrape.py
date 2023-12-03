ea553ff33f74a93a2e05a9f1b1af8151f58cfcbd
Voluptatemn <wqg1621@gmail.com>
2023-12-03 14:44:12 -0800

    comit

--- YSEScrape.py
+++ YSEScrape.py
@@ -3,8 +3,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
 
 YSEpath = "https://ziggy.ucolick.org/yse/login/?next=/yse/dashboard/"
+
+# payload = {
+#     'username': "hstacey",
+#     'password': "CDK700@Thacher"
+# }
+
 username = "hstacey"
 password = "CDK700@Thacher"
 delay = 100
@@ -14,6 +22,7 @@ driver.get(YSEpath)
 driver.find_element(By.NAME, "username").send_keys(username)
 driver.find_element(By.NAME, "password").send_keys(password)
 driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[3]/div/button').click()
+# table = driver.find_element(By.CLASS_NAME, 'table table-bordered table-hover')
 
 try:
     myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'table table-bordered table-hover')))
@@ -21,7 +30,27 @@ try:
 except TimeoutException:
     print ("Loading took too much time!")
     
-table = driver.find_element(By.ID, "k2_transient_tbl")
+# url = "https://ziggy.ucolick.org/yse/dashboard/"
+# r = requests.get(url)
+# parser = BeautifulSoup(r.content, "html.parser")
+# table = parser.find("table")
+
+# import mechanize
+# from bs4 import BeautifulSoup
+# import urllib3
+# import http.cookiejar
+
+# cj = http.cookiejar.CookieJar()
+# br = mechanize.Browser()
+# br.set_cookiejar(cj)
+# br.open(YSEpath)
+
+
+
+
+
+
+
 
 
 
