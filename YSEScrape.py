8b629324529d7e246538d6678c557390c4788f3b
Voluptatemn <wqg1621@gmail.com>
2023-12-03 22:06:10 -0800

    com

--- YSEScrape.py
+++ YSEScrape.py
@@ -1,12 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-import requests
-from bs4 import BeautifulSoup
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.common.by import By
+# import requests
+# from bs4 import BeautifulSoup
 # from selenium import webdriver
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
