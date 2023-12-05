62028de3245f844d19650a7bad6d1ff15bb9a6a4
Voluptatemn <wqg1621@gmail.com>
2023-12-05 09:56:19 -0800

    commit

--- YSEScrape.py
+++ YSEScrape.py
@@ -11,9 +11,8 @@ magnitude_min = 12
 min_declination = -10 # >=
 
 recent_discovery = 7 # days max
-today = DT.date.today()
-week_ago = today - DT.timedelta(days=recent_discovery)
-date_string = week_ago.strftime("%Y%m%d")
+today = DT.date.today() - DT.timedelta(days=recent_discovery)
+date_string = today.strftime("%Y%m%d")
 date_int = int(date_string)
 print(date_int)
 
@@ -36,31 +35,24 @@ session.auth = (username, password)
 
 response = session.get(api).json()
 while response['previous'] != None:
-
     
     results = response['results']
-    supernovea_date = results[0]['disc_date'][:10].replace('-', '')
+    supernovea_date = int(results[0]['disc_date'][:10].replace('-', ''))
     if supernovea_date >= date_int:
-        continue
+        for i in range (len(results)):
+                
+            result = results[i]
+                
+            try:
+                if result['redshift'] >= max_redshift:
+                    continue
+                if result['dec'] <= min_declination:
+                    continue
+            except TypeError:
+                continue
     else:
         response = session.get(response['previous']).json()
-        
-    
-    break
-    for i in range (len(results)):
-        
-        result = results[i]
-        
-        try:
-            if result['redshift'] >= max_redshift:
-                continue
-            if result['dec'] <= min_declination:
-                continue
-        except TypeError:
-            continue
-            
-
-
+   
     response = session.get(response['previous']).json()
 
 session.close()
