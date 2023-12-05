61784ef5ea4a658ee8df473d64e449764440c5ed
Voluptatemn <wqg1621@gmail.com>
2023-12-05 09:50:32 -0800

    commit

--- YSEScrape.py
+++ YSEScrape.py
@@ -1,8 +1,25 @@
 import requests
+import datetime as DT
 
-file = open("ysescrapingsetting.txt", "r+")
-content = file.read()
-page_starting = content
+# limit parameters for supernovea
+max_redshift = 0.015 # <=
+
+magnitude_max = 19
+
+magnitude_min = 12
+
+min_declination = -10 # >=
+
+recent_discovery = 7 # days max
+today = DT.date.today()
+week_ago = today - DT.timedelta(days=recent_discovery)
+date_string = week_ago.strftime("%Y%m%d")
+date_int = int(date_string)
+print(date_int)
+
+follow_up_request = True # prioritize follow up requested 
+
+potential_supernova = []
 
 username = "hstacey"
 password = "CDK700@Thacher"
@@ -12,33 +29,39 @@ payload = {
     'password': password
 }
 
-api = 'https://ziggy.ucolick.org/yse/api/transients/'
-
-if page_starting != '0':
-    api = page_starting
+api = 'https://ziggy.ucolick.org/yse/api/transients/?offset=148100'
 
 session = requests.Session()
 session.auth = (username, password)
 
 response = session.get(api).json()
-while response['next'] != None:
+while response['previous'] != None:
+
     
-    # year = response['results'][0]['name'][:4]
-    # try:
-    #     year = int(year)
-    # except:
-    #     response = session.get(response['next']).json()
-    #     continue
-    # print(response['previous'])
-    # if year == 2023:
-    #     file.truncate(0)
-    #     file.write(response['previous'])
-    #     file.close()
-    #     break
-    '''
-    Do something
-    '''
-    response = session.get(response['next']).json()
+    results = response['results']
+    supernovea_date = results[0]['disc_date'][:10].replace('-', '')
+    if supernovea_date >= date_int:
+        continue
+    else:
+        response = session.get(response['previous']).json()
+        
+    
+    break
+    for i in range (len(results)):
+        
+        result = results[i]
+        
+        try:
+            if result['redshift'] >= max_redshift:
+                continue
+            if result['dec'] <= min_declination:
+                continue
+        except TypeError:
+            continue
+            
+
+
+    response = session.get(response['previous']).json()
 
 session.close()
 
