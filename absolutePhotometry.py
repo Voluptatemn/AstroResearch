65b3a427947e33b80d5e56eb4cc9702070666260
Voluptatemn <wqg1621@gmail.com>
2023-12-07 13:13:34 -0800

    commit

--- absolutePhotometry.py
+++ absolutePhotometry.py
@@ -4,7 +4,7 @@ def finding_targ_magnitude(targ_counts, ref_counts, ref_mag):
     return ref_mag - 2.5 * np.log10(targ_counts/ref_counts)
 
 def mag_to_flux(mag, zp_mag = 24.76):
-    return 3631 * (10 ** (-0.4*(mag - zp_mag)))
+    return 3631 * (10 ** (-0.4*(mag)))
 
 def jy_to_W(flux):
     return flux * (10 ** -26)
@@ -22,6 +22,10 @@ print(np.mean(targ_flux), np.std(targ_flux))
 Aeff = np.pi * np.power((0.7/2), 2)
 c = 299792458
 h = 6.62607015 * (10 ** -34)
-photon_per_sec = targ_flux * Aeff * (1 - 0.47) * (703-545) * (10 ** -9) / c / h
+cen_ob = 1 - 0.47
+mid_point = ((703-545) / 2 + 545)
+photon_energy = c * h / (mid_point * (10 ** -9))
+delta_f = np.abs((c / 703 - c / 545)) * (10 ** -9)
+photon_per_sec = targ_flux * Aeff * cen_ob / photon_energy * delta_f
 print(np.mean(photon_per_sec), np.std(photon_per_sec))
 
