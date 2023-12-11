40a8939c0bb1ac633b1963aef50979d49975ed75
Voluptatemn <wqg1621@gmail.com>
2023-12-10 19:53:44 -0800

    commit

--- dachronicBayes.py
+++ dachronicBayes.py
@@ -8,14 +8,10 @@ pos_specificity = 0.096
 neg_likelihood = 0.904
 neg_specificy = 0.2
 
-prior = 0.01
-for i in range (4):
-    prior = bayes(prior, pos_likelihood, pos_specificity)
-    print(prior)
-
-
 prior = 0.08
 one_prior = bayes(1 - prior, neg_likelihood, neg_specificy)
 print(one_prior)
 two_prior = bayes(one_prior, neg_likelihood, neg_specificy)
 print(two_prior)
+three_prior = bayes(two_prior, neg_likelihood, neg_specificy)
+print(three_prior)
