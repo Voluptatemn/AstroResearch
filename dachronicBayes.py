4cf6025907b99a8c8ba2f612c3d7b89d6018ae0c
Voluptatemn <wqg1621@gmail.com>
2023-12-10 19:24:14 -0800

    commit

--- dachronicBayes.py
+++ dachronicBayes.py
@@ -1,10 +1,21 @@
 import numpy 
 
-def bayes(prior, sensitivity, specificity):
-    return sensitivity * prior / (sensitivity * prior + specificity * (1 - prior))
+def bayes(prior, likelihood, specificity):
+    return likelihood * prior / (likelihood * prior + specificity * (1 - prior))
 
-prior = 0.08 
-sensitivity = 0.8
-specificity = 0.096
-print(bayes(prior, sensitivity, specificity))
+pos_likelihood = 0.8
+pos_specificity = 0.096
+neg_likelihood = 0.904
+neg_specificy = 0.2
 
+prior = 0.01
+for i in range (4):
+    prior = bayes(prior, pos_likelihood, pos_specificity)
+    print(prior)
+
+
+prior = 0.08
+one_prior = bayes(1 - prior, neg_likelihood, neg_specificy)
+print(one_prior)
+two_prior = bayes(one_prior, neg_likelihood, neg_specificy)
+print(two_prior)
