26f80eefe8087c632c21f0414a8919cdaa6542f0
Voluptatemn <wqg1621@gmail.com>
2024-01-10 21:04:28 -0800

    commit

--- YSEScrape.py
+++ YSEScrape.py
@@ -1,96 +1,125 @@
 import requests
 import datetime as DT
+import time 
 
-# limit parameters for supernovea
-max_redshift = 0.015 # <=
+class YSE_worm:
+    
+    def __init__(self, max_redshift = 0.015, magnitude_max = 19, magnitude_min = 12, min_declination = -10, recent_discovery = 7, follow_up_request = True, username = "hstacey", password = "CDK700@Thacher", api = 'https://ziggy.ucolick.org/yse/api/transients/?offset=152400') -> None:
+        
+        self.max_redshift = max_redshift
+        self.magnitude_max = magnitude_max
+        self.magnitude_min = magnitude_min
+        self.min_declination = min_declination
+        self.recent_discovery = recent_discovery
+        self.follow_up_request = follow_up_request
+        self.username = username
+        self.password = password
+        self.api = api
+        
+    def dates(self):
+    
+        today = DT.date.today() - DT.timedelta(days=self.recent_discovery)
+        date_string = today.strftime("%Y%m%d")
+        date_int = int(date_string)
+        return date_int
+    
+    def scrape(self):
+        
+        start_time = time.time()
+    
+        potential_supernova = []
 
-magnitude_max = 19
+        session = requests.Session()
+        session.auth = (self.username, self.password)
 
-magnitude_min = 12
-
-min_declination = -10 # >=
-
-recent_discovery = 7 # days max
-today = DT.date.today() - DT.timedelta(days=recent_discovery)
-date_string = today.strftime("%Y%m%d")
-date_int = int(date_string)
-
-follow_up_request = True # prioritize follow up requested 
-
-potential_supernova = []
-
-username = "hstacey"
-password = "CDK700@Thacher"
-
-payload = {
-    'username': username,
-    'password': password
-}
-
-api = 'https://ziggy.ucolick.org/yse/api/transients/?offset=148100'
-
-session = requests.Session()
-session.auth = (username, password)
-
-response = session.get(api).json()
-
-while response['next'] != None:
-    response = session.get(response['next']).json()
-
-first_encounter = True
-while response['previous'] != None:
-
-    results = response['results']
-    supernovea_created_date = int(results[-1]['created_date'][:10].replace('-', ''))
-    if supernovea_created_date >= date_int:
-        for i in range (len(results)):
+        response = session.get(self.api).json()
+        print("Connect successfully")
+        
+        new_api = self.api
+        # goes to the back of the sequence if not in the back right now
+        while response['next'] != None:
+            new_api = response['next']
+            response = session.get(response['next']).json()
+        print("Successfully go to the end of the page")
+        print("Supernova names indicate the programming is looping through the potential supernova")
+        first_encounter = True
+        while response['previous'] != None:
             
-            result = results[i]
-            print(result['name'])
-            try:
-                redshift = result['redshift']
-                if redshift == None:
-                    host = session.get(result['host']).json()
-                    redshift = host['redshift']
-                    if redshift > max_redshift:
+            # check the page if the entire page is already greater than mimunim allowed dates
+            # use created dates because the page is ordered by created dates 
+            results = response['results']
+            supernovea_created_date = int(results[-1]['created_date'][:10].replace('-', ''))
+            date_int = self.dates()
+            if supernovea_created_date >= date_int:
+                for i in range (len(results)):
+                    
+                    result = results[i]
+                    print(result['name'])
+                    try:
+                        # check redshift if greater than the maxmimum redshift
+                        redshift = result['redshift']
+                        if redshift == None:
+                            host = session.get(result['host']).json()
+                            redshift = host['redshift']
+                            if redshift >= self.max_redshift:
+                                continue
+                        else:
+                            if redshift >= self.max_redshift:
+                                continue
+                        
+                        # check dec if less then minmum declination
+                        dec = result['dec']
+                        if dec < self.min_declination:
+                            continue
+                        
+                        # check if magnitude if less then min or greater than max
+                        magnitude = result['non_detect_limit']
+                        if magnitude < self.magnitude_min or magnitude > self.magnitude_max:
+                            continue
+                        
+                        # check if the discovery date is older then the allowed window
+                        supernova_discovery_date = int(result['disc_date'][:10].replace('-', ''))
+                        if supernovea_created_date < date_int:
+                            continue
+                        
+                        # create the return dict
+                        supernova_dict = {
+                            'name': result['name'],
+                            'ra': result['ra'],
+                            'dec': dec,
+                            'magnitude': magnitude,
+                            'redshift': redshift,
+                            'disc_date': result['disc_date']
+                        }
+                        potential_supernova.append(supernova_dict)
+                    except TypeError:
                         continue
+                    except requests.exceptions.MissingSchema:
+                        continue
+            # adding a potential fail safe for two consecutive older created dates garentees there is no more applicable supernova to search
+            else:
+                if first_encounter:
+                    response = session.get(response['previous']).json()
+                    first_encounter = False
                 else:
-                    if redshift > max_redshift:
-                        continue
-                dec = result['dec']
-                if dec < min_declination:
-                    continue
-                magnitude = result['non_detect_limit']
-                if magnitude < magnitude_min or magnitude > magnitude_max:
-                    continue
-                supernova_discovery_date = int(result['disc_date'][:10].replace('-', ''))
-                if supernovea_created_date < date_int:
-                    continue
-                supernova_dict = {
-                    'name': result['name'],
-                    'ra': result['ra'],
-                    'dec': dec,
-                    'magnitude': magnitude,
-                    'redshift': redshift,
-                    'disc_date': result['disc_date']
-                }
-                potential_supernova.append(supernova_dict)
-            except TypeError:
-                continue
-            except requests.exceptions.MissingSchema:
-                continue
-    else:
-        if first_encounter:
+                    break
+            # move the session
             response = session.get(response['previous']).json()
-            first_encounter = False
-        else:
-            break
-   
-    response = session.get(response['previous']).json()
 
-session.close()
+        session.close()
 
-print(potential_supernova)
+        print(potential_supernova)
+        
+        end_time = time.time()
+        elapsed_time_seconds = end_time - start_time
+        elapsed_minutes = int(elapsed_time_seconds // 60)
+        elapsed_seconds = int(elapsed_time_seconds % 60)
+
+        print(f"Job complete, Elapsed time: {elapsed_minutes} minutes and {elapsed_seconds} seconds")
+        
+        return potential_supernova, new_api
+
+
 
 
 
--- worm_main.py
+++ worm_main.py
@@ -1,5 +1,15 @@
 from crontab import CronTab
-cron = CronTab(user=True)
-job = cron.new(command='echo hello_world')
-cron.write() 
+from YSEScrape import YSE_worm
+
+def add_cron_job(command, user = "Thacher", schedule = '0 * * * *'):
+    cron = CronTab(user)  # Specify the username if necessary
+
+    job = cron.new(command=command)
+    job.setall(schedule)
+
+    cron.write()
+
+yse_worm = YSE_worm()
+add_cron_job(command = f'python -c "from YSEScrape import YSE_worm; YSE_worm.scrape()"')
+
 
