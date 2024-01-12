fda2316e9c5974f3b53d933ac1bd2af026b03dbe
Voluptatemn <wqg1621@gmail.com>
2024-01-11 19:36:46 -0800

    commit

--- worm_main.py
+++ worm_main.py
@@ -1,8 +1,8 @@
 from crontab import CronTab
 from YSEScrape import YSE_worm
 
-def add_cron_job(command, user = "qiangangsamwang@QiangangdeMacBook-Pro", schedule = '0 * * * *'):
-    cron = CronTab(user)  # Specify the username if necessary
+def add_cron_job(command, schedule = '0 * * * *'):
+    cron = CronTab(user=True)  # Specify the username if necessary
 
     job = cron.new(command=command)
     job.setall(schedule)
