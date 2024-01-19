f5036bf4b0cf7ef0ec8d3dfb1c5de3589f846e4c
Voluptatemn <wqg1621@gmail.com>
2024-01-19 11:26:57 -0800

    commit

--- mcmclinearhw.py
+++ mcmclinearhw.py
@@ -99,7 +99,7 @@ def metropolis_hasting(start_pointing, counts = {}, num_of_tracktors = 0, m_mean
     #             print(num_of_tracktors)
     #     else:
     #         print(num_of_tracktors)
-        previous = possibility_of_model_given_data(current_pointing[0], current_pointing[1], fast_version)
+        previous = possibility_of_model_given_data(current_pointing[0], current_pointing[1], fast_version = fast_version)
         
         # draw form distribution for interval 
         m_move = np.random.normal(m_mean, m_std)
@@ -107,7 +107,7 @@ def metropolis_hasting(start_pointing, counts = {}, num_of_tracktors = 0, m_mean
         
         # change the m and b
         aftter_pointing = current_pointing + (m_move, b_move)
-        after = possibility_of_model_given_data(aftter_pointing[0], aftter_pointing[1], fast_version)
+        after = possibility_of_model_given_data(aftter_pointing[0], aftter_pointing[1], fast_version = fast_version)
         
         # acceptance prob
         acceptance_prob = 0
